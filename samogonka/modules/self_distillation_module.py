fix KL div

samogonka/modules/self_distillation_module.py
@@ -39,13 +39,15 @@ class SelfDistillationModule(ClassificationModule):
                 logits = pred / self.T
                 info[f'CE-{layer_num}'] = self.criterion(logits, labels)
                 info[f'ACC-{layer_num}'] = self.accuracy_metric(logits, labels)
+                if layer_num != 4:
+                    deepest_feats = predicts['logits4']
+                    info[f'KL-{layer_num}'] = self._kl(pred / self.T, deepest_feats / self.T)
                 continue
             if layer_num == 4:
                 continue
             assert 'layer' in pred_name
             deepest_feats = predicts['layer4']
             info[f'MSE-F{layer_num}'] = self.mse_loss(pred, deepest_feats)
-            info[f'KL-{layer_num}'] = self._kl(pred / self.T, deepest_feats / self.T)
 
         info['KL'] = info['KL-1'] + info['KL-2'] + info['KL-3']
         info['MSE-F'] = info['MSE-F1'] + info['MSE-F2'] + info['MSE-F3']
@@ -65,10 +67,8 @@ class SelfDistillationModule(ClassificationModule):
         # KL(q_s || q_t) = sum [q_s log q_s - q_s log p_t]
         log_q_s = torch.nn.functional.log_softmax(student_logits, -1)
         log_q_t = torch.nn.functional.log_softmax(teacher_logits, -1)
-
-        q_s = torch.exp(log_q_s)
-
-        return torch.mean(torch.sum(q_s * (log_q_s - log_q_t), dim=-1))
+        # return torch.nn.functional.kl_div(log_q_s, log_q_t, reduction='batchmean', log_target=True)
+        return torch.mean(torch.sum(torch.exp(log_q_s) * (log_q_s - log_q_t), dim=-1))
 
     def _log_info(self, info, prefix):
         for k, v in info.items():