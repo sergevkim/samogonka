99e0e3361df4cb35ad584156bfb18e73e50702e8
Serge Kim <sergevkim@gmail.com>
2022-10-16 22:28:26 +0300

    hydra (student from scratch)

--- scripts/train_student_from_scratch.py
+++ scripts/train_student_from_scratch.py
@@ -1,25 +1,20 @@
 from argparse import ArgumentParser
 
+import hydra
+from hydra.utils import instantiate
+from omegaconf import DictConfig
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks import ModelCheckpoint
-from pytorch_lightning.loggers import WandbLogger
-from samogonka.datamodules import CIFAR10DataModule
-from samogonka.models import ResNet18Model
-from samogonka.modules import ClassificationModule
 
 
-def main(args):
+@hydra.main(version_base=None, config_path='../conf', config_name='student_from_scratch')
+def main(cfg: DictConfig) -> None:
     seed_everything(9, workers=True)
 
-    #model = ResNet18Model()
-    from torchvision.models import resnet18
-    import torch.nn as nn
-    model = resnet18()
-    model.fc = nn.Linear(512, 10, bias=True)
-
-    module = ClassificationModule(model=model, learning_rate=0.001)
-    datamodule = CIFAR10DataModule(batch_size=2048)
-    logger = WandbLogger(project='samogonka')
+    model = instantiate(cfg.model)
+    module = instantiate(cfg.module, model=model)
+    datamodule = instantiate(cfg.datamodule)
+    logger = instantiate(cfg.logger)
     checkpoint_callback = ModelCheckpoint(
         save_top_k=3,
         monitor='val_accuracy',
@@ -27,8 +22,7 @@ def main(args):
         dirpath='checkpoints',
         filename='student-nodis-{epoch:02d}-{val_accuracy:.2f}',
     )
-    trainer = Trainer.from_argparse_args(
-        args,
+    trainer = Trainer(
         max_epochs=40,
         accelerator='gpu',
         logger=logger,
@@ -39,7 +33,4 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser = Trainer.add_argparse_args(parser)
-    args = parser.parse_args()
-    main(args)
+    main()
